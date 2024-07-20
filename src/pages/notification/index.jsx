5d8e7dc685880ee7ef5b080ef80cd9ca0f01ad32
Ansalos <tonybajjali7@gmail.com>
2024-07-21 01:48:29 +0300

    notification

--- src/pages/notification/index.jsx
+++ src/pages/notification/index.jsx
@@ -5,6 +5,7 @@
     import { useRegister } from '../../hooks/useRegister';
     import { doc, deleteDoc, getDocs, collection, getDoc, writeBatch, where } from 'firebase/firestore';
     import { getAuth } from 'firebase/auth';
+    import './styles.css';
     
     export const Notifications = () => {
         const toGetAuth = getAuth();
@@ -26,8 +27,8 @@
             uid: ''
             }
         );
-        
-        
+        const [selectedRequest, setSelectedRequest] = useState(null);
+        const [selectedProjectUser, setSelectedProjectUser] = useState(null);
         useEffect(() => {
             
             setLoading(true);
@@ -47,6 +48,9 @@
                         ]);
                         if (userDoc.exists()) {
                             const userData = userDoc.data();
+                            if (userData.role === 'Guest'){
+                                navigate('/home');
+                            }
                             setUserDetails({
                                 userId: userData.id || '',
                                 firstName: userData.firstName || '',
@@ -59,6 +63,7 @@
                         } else {
                             console.error('User document not found');
                         }
+                        
                         const projectsList = [];
                         projectDocs.forEach((doc) => {
                             projectsList.push({ id: doc.id, ...doc.data() });
@@ -165,63 +170,55 @@
             await deleteNotification(notify, false, false);
         }
     
-        const renderRegisterListAdmin = () => { 
+        const renderRegisterListAdmin = () => {
             const userNotifications = acceptanceList.filter(acceptance => acceptance.user_uid === userDetails.uid);
             let registerTo = "";
             let notificationUidAdmin = "";
-            
+        
             for (const notification of userNotifications) {
                 registerTo = notification.registTo;
-                if (notification.registTo === 'Admin'){
+                if (notification.registTo === 'Admin') {
                     notificationUidAdmin = notification.id;
-                      
                 }
-                
-            }     
+            }
             const render = renderRegistProjects();
+        
+            const hasRegisterListData = registerList.length > 0;
+            const hasProjectListData = notifies.filter(notify => notify.workerID.includes(userDetails.uid)).length > 0;
+        
+            if (acceptanceList.length === 0 && !hasRegisterListData && !hasProjectListData) {
+                return <p className="notification-message">No notifications</p>;
+            }
+        
             return (
                 <>
-                {acceptanceList.length === 0 ? (
-                    <>
-                        {render}    
-                    </>
-                ):(
-                    <>  
-                        {registerTo !== "" && (
-                            <div>
-                                {(notificationAdminDeleted === false) && notificationUidAdmin && (
-                                        <>
-                                            <h3>the request is accepted to change the role to: {registerTo}</h3>
-                                            <button onClick={() => deleteNotification(notificationUidAdmin, true, true)}>close</button>                                        
-                                        </>
-                                )} 
-                            </div>
+                    
+                    <div className={`tables-container ${!hasRegisterListData || !hasProjectListData ? 'single-table-container' : ''}`}>
+                        {hasRegisterListData && (
+                            <table className="register-list-table">
+                                <thead className="register-list-thead">
+                                    <tr className="register-list-thead-tr">
+                                        <th className="register-list-th">First Name</th>
+                                        <th className="register-list-th">Last Name</th>
+                                        <th className="register-list-th">Register to</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="register-list-tbody">
+                                    {registerList.map((register) => (
+                                        <tr className="register-list-tbody-tr" key={register.id} onClick={() => setSelectedRequest(register)}>
+                                            <td className="register-list-td">{register.firstName}</td>
+                                            <td className="register-list-td">{register.lastName}</td>
+                                            <td className="register-list-td">{register.registTo}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         )}
-                    </>
-                )}
-                {registerList.length === 0 ? (
-                    <p>No pending registrations.</p>
-                ) : (
-                    <div className="register-list">
-                        {registerList.map((register) => (
-                            <div key={register.id} className="register-item">
-                                <p><strong>User ID:</strong> {register.userId}</p>
-                                <p><strong>First Name:</strong> {register.firstName}</p>
-                                <p><strong>Last Name:</strong> {register.lastName}</p>
-                                <p><strong>Role:</strong> {register.role}</p>
-                                <p><strong>PhoneNumber:</strong> {register.phoneNumber}</p>
-                                <p><strong>Register to:</strong> {register.registTo}</p>
-    
-                                <button onClick={() => acceptUser(register.id)}>Accept</button>
-                                <button onClick={() => rejectUser(register.id)}>Reject</button>
-                            </div>
-                        ))}
+                        {hasProjectListData && render}
                     </div>
-                )}
                 </>
-            )
-        }
-    
+            );
+        };
         const deleteNotification = async (notificationId, isAdmin, isAcceptance) => {
             if(isAdmin && isAcceptance){
                 setNotificationAdminDeleted(true);
@@ -263,15 +260,8 @@
             }
             const render = renderRegistProjects();
             return (
-               <> 
-                acceptanceList.length === 0 ? (
-                    <>
-                        <p>There is no any Notifications</p>
-                        {render}
-                        
-                    </>
-                ):(
                     <>  
+                        {render}
                         {registerTo !== "" && (
                             <div>
                                 {(notificationWorkerDeleted === false) && notificationUidWorker && (
@@ -285,10 +275,6 @@
                         
                     </>
                 )
-                
-                </>
-            )
-            
         }
     
         const renderProjectUsersDetails = (projectId, userID, notify) => {
@@ -297,60 +283,155 @@
             const userDetails = users.filter(user => user.uid === userID);
             const user = userDetails[0];
             if (!project || !user) {
-              return null; // Return null if project or user not found
+                return null; // Return null if project or user not found
             }
-            
-            return (
-              <div key={`${project.id}-${user.uid}`} className="register-item-project">
-                <h3>Project Details</h3>
-                <p><strong>ProjectTitle: </strong> {project.projectTitle}</p>
-                <p><strong>Description: </strong> {project.description}</p>
-                <p><strong>StartDate: </strong> {project.startDate}</p>
-                <p><strong>EndDate: </strong> {project.endDate}</p>
-                <h3>User Details</h3>
-                <p><strong>First Name: </strong> {user.firstName}</p>
-                <p><strong>Last Name: </strong> {user.lastName}</p>
-                <p><strong>Location: </strong> {user.location}</p>
-                <p><strong>Email: </strong> {user.email} </p>
-                <p><strong>PhoneNumber: </strong> {user.phoneNumber}</p>
-                <button onClick={() => acceptUserProject(user.uid,project.id,notify.id)}>Accept</button>
-                <button onClick={() => deleteNotification(notify.id, true, false)}>Reject</button>
-              </div>
-            );
-          };
-    
         
-          const renderRegistProjects = () => {
-            const projectElements = notifies
-              .filter(notify => notify.workerID.includes(userDetails.uid))
-              .map(notify => renderProjectUsersDetails(notify.projectId, notify.userId, notify))
-              .filter(projectElement => projectElement !== null); // Filter out null elements
             return (
-              <div>
-                {projectElements.length > 0 ? projectElements : <p>No projects found.</p>}
-              </div>
+                <tr className="register-list-tbody-tr" key={`${project.id}-${user.uid}`} onClick={() => setSelectedProjectUser({ project, user, notify })}>
+                    <td className="register-list-td">{user.firstName}</td>
+                    <td className="register-list-td">{user.lastName}</td>
+                    <td className="register-list-td">{project.projectTitle}</td>
+                </tr>
             );
-          };
-    
-        const handleBack = () => {
-            navigate('/home'); // Navigate back to the homepage
         };
-    
+        
+        const renderRegistProjects = () => {
+            const projectElements = notifies
+                .filter(notify => notify.workerID.includes(userDetails.uid))
+                .map(notify => renderProjectUsersDetails(notify.projectId, notify.userId, notify))
+                .filter(projectElement => projectElement !== null); // Filter out null elements
+        
+            return (
+                <table className="register-list-table">
+                    <thead className="register-list-thead">
+                        <tr className="register-list-thead-tr">
+                            <th className="register-list-th">First Name</th>
+                            <th className="register-list-th">Last Name</th>
+                            <th className="register-list-th">Project Title</th>
+                        </tr>
+                    </thead>
+                    <tbody className="register-list-tbody">
+                        {projectElements.length > 0 ? projectElements : (
+                            <tr className="register-list-tbody-tr">
+                                <td className="register-list-td" colSpan="3">No projects found.</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            );
+        };
+        
         return (
-            <div className="notifications">
-                <header className="header">
-                    <button onClick={handleBack}>Back</button>
-                    <h1>Pending Registrations</h1>
-                </header>
-                <main className="main-content">
-                    {userDetails.role === 'Admin' ? (
-                        renderRegisterListAdmin()
-                    ):(
-                        renderRegisterListWorker()     
-                    )}
-                </main>
+            <div className="notification-list">
+                {userDetails.role === 'Admin' && renderRegisterListAdmin()}
+                {userDetails.role === 'Worker' && renderRegisterListWorker()}
+    
+                {selectedRequest && (
+                    <div className="popup-overlay">
+                        <div className="popup-content">
+                            <h3>Request Details</h3>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td><strong>User ID:</strong></td>
+                                        <td>{selectedRequest.userId}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>First Name:</strong></td>
+                                        <td>{selectedRequest.firstName}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Last Name:</strong></td>
+                                        <td>{selectedRequest.lastName}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Role:</strong></td>
+                                        <td>{selectedRequest.role}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Phone Number:</strong></td>
+                                        <td>{selectedRequest.phoneNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Register to:</strong></td>
+                                        <td>{selectedRequest.registTo}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div className="popup-buttons">
+                                <button onClick={() => acceptUser(selectedRequest.id)}>Accept</button>
+                                <button onClick={() => rejectUser(selectedRequest.id)}>Reject</button>
+                                <button onClick={() => setSelectedRequest(null)}>Close</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+    
+                {selectedProjectUser && (
+                      <div className="popup-overlay">
+                      <div className="popup-content">
+                          <h3>Project Details</h3>
+                          <table>
+                              <tbody>
+                                  <tr>
+                                      <td><strong>Project Title:</strong></td>
+                                      <td>{selectedProjectUser.project.projectTitle}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>Description:</strong></td>
+                                      <td>{selectedProjectUser.project.description}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>Start Date:</strong></td>
+                                      <td>{selectedProjectUser.project.startDate}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>End Date:</strong></td>
+                                      <td>{selectedProjectUser.project.endDate}</td>
+                                  </tr>
+                                   </tbody>
+                                  </table>
+                                  <h3>User Details</h3>
+                                  <table>
+                                  <tbody>
+                                 
+                                  <tr>
+                                      <td><strong>First Name:</strong></td>
+                                      <td>{selectedProjectUser.user.firstName}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>Last Name:</strong></td>
+                                      <td>{selectedProjectUser.user.lastName}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>Location:</strong></td>
+                                      <td>{selectedProjectUser.user.location}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>Email:</strong></td>
+                                      <td>{selectedProjectUser.user.email}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>Phone Number:</strong></td>
+                                      <td>{selectedProjectUser.user.phoneNumber}</td>
+                                  </tr>
+                                  <tr>
+                                      <td><strong>Role:</strong></td>
+                                      <td>{selectedProjectUser.user.role}</td>
+                                  </tr>
+                              </tbody>
+                          </table>
+                          <div className="popup-buttons">
+                              <button onClick={() => acceptUserProject(selectedProjectUser.user.uid, selectedProjectUser.project.id, selectedProjectUser.notify.id)}>Accept</button>
+                              <button onClick={() => deleteNotification(selectedProjectUser.notify.id, true, false)}>Reject</button>
+                              <button onClick={() => setSelectedProjectUser(null)}>Close</button>
+                          </div>
+                      </div>
+                  </div>
+                )}
             </div>
         );
     };
     
     export default Notifications;
+    
