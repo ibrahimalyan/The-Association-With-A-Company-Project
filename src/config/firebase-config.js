2bb15412d40566c30b7868d2ffa22e6597f093be
Ansalos <tonybajjali7@gmail.com>
2024-07-20 03:53:20 +0300

    firebase-config

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -14,6 +14,16 @@ const firebaseConfig = {
   appId: "1:56464057307:web:6c9af4d9528bea8045fdfe"
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAWqoxnr7hBd4RiFFJiAXTrPeoXPGwViqM",
+//   authDomain: "company-745bf.firebaseapp.com",
+//   projectId: "company-745bf",
+//   storageBucket: "company-745bf.appspot.com",
+//   messagingSenderId: "1043563457995",
+//   appId: "1:1043563457995:web:1b61b709b60ddb55271a4e",
+//   measurementId: "G-WEX0XXR8NJ"
+// };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
